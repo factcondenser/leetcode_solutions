f6d2b12548bf1d736f1218731157b476f667e55f
factcondenser <markpan@live.com>
2018-09-07 09:25:21 -0500

    Initial ruby soln for prob 3

---
+++ 3_longest_substring.rb
@@ -0,0 +1,22 @@
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)
+  hsh = {}
+  length = 0
+  maxlength = 0
+  
+  s.split('').each_with_index do |c, i|
+    if hsh.has_value? c
+      new_start = hsh.key(c) + 1
+      new_keys = [*new_start..i]
+      length = new_keys.length
+      hsh = Hash[[new_keys, hsh.values_at(*new_keys)].transpose]
+    else
+      length += 1
+      maxlength = length if length > maxlength
+    end
+    hsh[i] = c
+  end
+  
+  maxlength
+end
