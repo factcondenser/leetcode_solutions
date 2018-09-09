2277b8471593fb0ce6dcbfab880822f8a6f87a5c
factcondenser <markpan@live.com>
2018-09-09 09:18:12 -0500

    Fix typos in prob 5 ruby soln

--- 5_longest_palindromic_substring.rb
+++ 5_longest_palindromic_substring.rb
@@ -7,13 +7,13 @@ def longest_palindrome(s)
   result = ''
   res_len = 0
 
-  (0..s_len).each do |i|
+  (0...s_len).each do |i|
     len1 = expand_around_centers(s, i, i)
     len2 = expand_around_centers(s, i, i + 1)
     len = [len1, len2].max
 
     if len > res_len
-      result = s[i - (len - 1)/2, len]
+      result = s[i - (len - 1) / 2, len]
       res_len = len
     end
   end
