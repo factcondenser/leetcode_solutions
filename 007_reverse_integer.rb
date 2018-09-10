c8ecfe4f7c3e22a889c808c799e5f2bf13d483c0
factcondenser <markpan@live.com>
2018-09-10 09:57:51 -0500

    Add faster ruby-esque ruby solution (that doesn't display thought process as explicitly)

--- 007_reverse_integer.rb
+++ 007_reverse_integer.rb
@@ -1,23 +1,32 @@
 # @param {Integer} x
 # @return {Integer}
+# def reverse(x)
+#   rev = 0
+#   int_max = 2**31
+#   int_min = -(2**31)
+#   while x != 0
+#     pop = x % 10
+#     if x > 0
+#       x /= 10
+#     else
+#       # Ruby's % behaves differently than Java's; could also use Integer#remainder
+#       pop = x % 10
+#       pop -= 10 unless pop == 0
+#       # Ruby's divide returns the floor for negatives
+#       x = (x / 10.0).ceil
+#     end
+#     return 0 if (rev > int_max / 10) || (rev == int_max / 10 && pop > 7) # int_max % 10
+#     return 0 if (rev < (int_min / 10.0).ceil) || (rev == (int_min / 10.0).ceil && pop < -8) # -(int_min % 10)
+#     rev = rev * 10 + pop
+#   end
+#   rev
+# end
+
+# @param {Integer} x
+# @return {Integer}
 def reverse(x)
-  rev = 0
-  int_max = 2**31
-  int_min = -(2**31)
-  while x != 0
-    pop = x % 10
-    if x > 0
-      x /= 10
-    else
-      # Ruby's % behaves differently than Java's; could also use Integer#remainder
-      pop = x % 10
-      pop -= 10 unless pop == 0
-      # Ruby's divide returns the floor for negatives
-      x = (x / 10.0).ceil
-    end
-    return 0 if (rev > int_max / 10) || (rev == int_max / 10 && pop > 7) # int_max % 10
-    return 0 if (rev < (int_min / 10.0).ceil) || (rev == (int_min / 10.0).ceil && pop < -8) # -(int_min % 10)
-    rev = rev * 10 + pop
-  end
-  rev
+  converted = x.to_s.reverse.to_i
+  return 0 if converted > (2**31) - 1 || converted < -2**31
+  return converted *= -1 if x < 0
+  converted
 end
