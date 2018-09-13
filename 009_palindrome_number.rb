4d719467644f95b0a20b1b55b50b995e372660e5
factcondenser <markpan@live.com>
2018-09-12 21:51:14 -0500

    Add more ruby-esque ruby soln for prop 9

--- 009_palindrome_number.rb
+++ 009_palindrome_number.rb
@@ -1,14 +1,20 @@
 # @param {Integer} x
 # @return {Boolean}
-def is_palindrome(x)
-  return false if x < 0
-  tmp = x
-  rev = 0
-  while tmp > 0
-    pop = tmp % 10
-    tmp /= 10
+# def is_palindrome(x)
+#   return false if x < 0
+#   tmp = x
+#   rev = 0
+#   while tmp > 0
+#     pop = tmp % 10
+#     tmp /= 10
 
-    rev = rev * 10 + pop
-  end
-  x == rev
+#     rev = rev * 10 + pop
+#   end
+#   x == rev
+# end
+
+# @param {Integer} x
+# @return {Boolean}
+def is_palindrome(x)
+  x >= 0 && x.to_s == x.to_s.reverse
 end
