d02a270eaae13aa23251f874fb3799c80c22d86c
factcondenser <markpan@live.com>
2018-09-15 22:44:03 -0500

    Use better hash keys and make style edits

--- 010_regex_matching.rb
+++ 010_regex_matching.rb
@@ -4,21 +4,22 @@
 def is_match(s, p)
   hsh = {}
   dp = lambda do |i, j|
-    unless hsh.key? [i, j]
+    hsh[i] = {} if hsh[i].nil?
+    if hsh[i][j].nil?
       if j == p.length
         ans = i == s.length
       else
         first_match = i < s.length && [s[i], '.'].include?(p[j])
-        if j + 1 < p.length && p[j + 1] == '*'
-          ans = dp.call(i, j + 2) || first_match && dp.call(i + 1, j)
-        else
-          ans = first_match && dp.call(i + 1, j + 1)
-        end
+        ans =
+          if j + 1 < p.length && p[j + 1] == '*'
+            dp.call(i, j + 2) || first_match && dp.call(i + 1, j)
+          else
+            first_match && dp.call(i + 1, j + 1)
+          end
       end
-      puts hsh.inspect
-      hsh[[i, j]] = ans
+      hsh[i][j] = ans
     end
-    hsh[[i, j]]
+    hsh[i][j]
   end
   dp.call(0, 0)
 end
