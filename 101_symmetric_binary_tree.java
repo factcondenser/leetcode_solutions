318b6db8f78768ed135bbcb7b895f6b9c5c6c274
factcondenser <markpan@live.com>
2018-12-27 21:32:36 -0600

    Add java soln for 101

---
+++ 101_symmetric_binary_tree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public boolean isSymmetric(TreeNode root) {
+      if (root == null) return true;
+  
+      return isSymmetric(root.left, root.right);
+  }
+  
+  private boolean isSymmetric(TreeNode left, TreeNode right) {
+      if (left == null || right == null) {
+          return (left == null && right == null);
+      }
+      
+      return (left.val == right.val)
+          && isSymmetric(left.left, right.right)
+          && isSymmetric(left.right, right.left);
+  }
+}
